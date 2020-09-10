be9bbf8177854c8e1afee39a33dd2edfa7dd46c6
Kavya Jain <jainkavya6890@gmailcom>
2020-09-10 14:06:08 +0000

    Database commit

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ var express 		= require("express"),
 var app				= express();
 //mon.connect("mongodb://localhost/Campground");
 
-mon.connect("mongodb+srv://campground:campground@campground.wkcs8.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+mon.connect("mongodb+srv://campground:campground@campground.wkcs8.mongodb.net/campground?retryWrites=true&w=majority", {
 	useNewUrlParser : true,
 	useCreateIndex : true
 	
